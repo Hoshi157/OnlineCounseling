ae3f57d4345cc34058e9f0196a53a56285399aa2
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-05-16 15:34:34 +0900

    メッセージカスタムセル作成。

---
+++ OnlineCounseling/View/TimelineTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimelineTableViewCell.swift
+//  OnlineCounseling
+//
+//  Created by 福山帆士 on 2020/05/16.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
