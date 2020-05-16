8f265297db430ee6ecfc6e5e6fbd97565930ff64
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-05-17 02:38:04 +0900

    customTableCell追加。

---
+++ OnlineCounseling/View/CustomTextTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTextTableViewCell.swift
+//  OnlineCounseling
+//
+//  Created by 福山帆士 on 2020/05/17.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
