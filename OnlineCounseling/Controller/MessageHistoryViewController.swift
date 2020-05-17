2200d3e5fa9ec331c09da8a0100e8fd123ba9774
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-05-17 21:30:37 +0900

    ストーリーボードの細かな調整。

--- OnlineCounseling/Controller/MessageHistoryViewController.swift
+++ OnlineCounseling/Controller/MessageHistoryViewController.swift
@@ -2,13 +2,13 @@
 //  MessageViewController.swift
 //  OnlineCounseling
 //
-//  Created by 福山帆士 on 2020/05/17.
+//  Created by 福山帆士 on 2020/05/14.
 //  Copyright © 2020 福山帆士. All rights reserved.
 //
 
 import UIKit
 
-class MessageViewController: UIViewController {
+class MessageHistoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ OnlineCounseling/Controller/MockMessage.swift
@@ -0,0 +1,13 @@
+//
+//  MockMessage.swift
+//  OnlineCounseling
+//
+//  Created by 福山帆士 on 2020/05/17.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import Foundation
+
+class MockMessage {
+    
+}
