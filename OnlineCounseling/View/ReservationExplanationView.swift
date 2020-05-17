cf73866670f9c74599993fa198a19355aba9c676
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-05-17 17:08:49 +0900

    カスタムView続き。

---
+++ OnlineCounseling/View/ReservationExplanationView.swift
@@ -0,0 +1,21 @@
+//
+//  ReservationExplanationView.swift
+//  OnlineCounseling
+//
+//  Created by 福山帆士 on 2020/05/17.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class ReservationExplanationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
