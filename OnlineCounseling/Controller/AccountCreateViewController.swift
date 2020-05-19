80bd9170676afd2d6184955afc6ffd60fd82703c
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-05-19 16:40:39 +0900

    アカウント作成画面にDatePicker表示。処理未実装。

--- OnlineCounseling/Controller/AccountCreateViewController.swift
+++ OnlineCounseling/Controller/AccountCreateViewController.swift
@@ -16,10 +16,46 @@ class AccountCreateViewController: UIViewController {
     @IBOutlet weak var consentButton: MDCRaisedButton!
     @IBOutlet weak var tableView: UITableView!
     
-    var tableArray = ["名前", "生年月日"]
+    private let tableArray = ["名前", "生年月日"]
+    
+    lazy var datePickerView: UIDatePicker = {
+       let picker = UIDatePicker()
+        picker.backgroundColor = .white
+        picker.locale = Locale.current
+        picker.timeZone = NSTimeZone.local
+        picker.datePickerMode = UIDatePicker.Mode.date
+        picker.frame = CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: self.view.frame.height * 0.25)
+        return picker
+    }()
+    
+    lazy var pickerToolbar: UIToolbar = {
+       let toolBar = UIToolbar()
+        toolBar.frame = CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: 40)
+        return toolBar
+    }()
+    
+    lazy var donePickerButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: "完了", style: .done, target: self, action: #selector(donePickerAction))
+        return button
+    }()
+    
+    lazy var canselPickerButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: "キャンセル", style: .plain, target: self, action: #selector(canselPickerAction))
+        return button
+    }()
+    
+    lazy var flexble: UIBarButtonItem = {
+        let spece = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: self, action: nil)
+        spece.width = self.view.frame.width * 0.6
+        return spece
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(datePickerView)
+        view.addSubview(pickerToolbar)
+        pickerToolbar.items = [canselPickerButton, flexble, donePickerButton]
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "icons8-ダブル左-25"), landscapeImagePhone: #imageLiteral(resourceName: "icons8-ダブル左-25"), style: .plain, target: self, action: #selector(backViewAction))
 
@@ -39,6 +75,14 @@ class AccountCreateViewController: UIViewController {
     @IBAction func consentButtonAction(_ sender: Any) {
     }
     
+    @objc func donePickerAction() {
+        
+    }
+    
+    @objc func canselPickerAction() {
+        
+    }
+    
 
     /*
     // MARK: - Navigation
@@ -55,7 +99,6 @@ class AccountCreateViewController: UIViewController {
 extension AccountCreateViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(tableArray.count)
         return tableArray.count
     }
     
@@ -80,13 +123,15 @@ extension AccountCreateViewController: UITableViewDelegate, UITableViewDataSourc
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        print(indexPath.row)
         switch (indexPath.row) {
         case 0:
             let textInputVC = self.storyboard?.instantiateViewController(withIdentifier: "textInputVC") as! TextInputProfileViewController
             self.navigationController?.pushViewController(textInputVC, animated: true)
         case 1:
-            print("生年月日")
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveLinear, animations: {
+                self.datePickerView.frame = CGRect(x: 0, y: self.view.frame.height - self.view.frame.height * 0.25, width: self.view.frame.width, height: self.view.frame.height * 0.25)
+                self.pickerToolbar.frame = CGRect(x: 0, y: self.view.frame.height - self.view.frame.height * 0.25 - 40, width: self.view.frame.width, height: 40)
+            }, completion: nil)
         default:
             print("error")
             return
