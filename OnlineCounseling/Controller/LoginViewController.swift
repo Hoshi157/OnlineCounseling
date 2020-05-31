442d91556c0323f5bc561fd92ded78806142a5ff
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-05-31 23:15:50 +0900

    メールアドレス、パスワード認証追加。

--- OnlineCounseling/Controller/LoginViewController.swift
+++ OnlineCounseling/Controller/LoginViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
     private let tableviewArray: [String] = ["メールアドレス", "パスワード", "パスワードの確認"]
+    private let alert = AlertController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +37,37 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonAction(_ sender: Any) {
+        let mailCell = self.tableView.cellForRow(at: [0,0]) as! CustomAccountTakeOverTableViewCell
+        let passwordCell = self.tableView.cellForRow(at: [0,1]) as! CustomAccountTakeOverTableViewCell
+        let confirmationPassCell = self.tableView.cellForRow(at: [0,1]) as! CustomAccountTakeOverTableViewCell
+        
+        if let adress = mailCell.textField.text, let password = passwordCell.textField.text, let confimationPass = confirmationPassCell.textField.text {
+        // パスワード欄と確認用パスワードの一致確認
+        if (password == confimationPass) {
+            alert.okAlert(title: "パスワードが正しくありません", message: "パスワードと確認用パスワードが一致しませんでした", currentController: self)
+            return
+        }
+        // 入力されているか確認
+        if (adress.isEmpty || password.isEmpty || confimationPass.isEmpty) {
+            alert.okAlert(title: "入力されていません", message: "アドレス、パスワードを入力してください", currentController: self)
+            return
+        }
+            // 以前ログインしたユーザーをログインさせる(ログイン成功後、Firebaseから自分の情報を引っ張ってくる　未実装)
+            Auth.auth().signIn(withEmail: adress, password: password) { (authResult, error) in
+                if (error != nil) {
+                    print(error!.localizedDescription)
+                    self.alert.okAlert(title: "ログインできません", message: "エラーが発生しログインできません", currentController: self)
+                    return
+                }
+                guard let user = authResult?.user else {
+                    return
+                }
+                print(user.uid, "user.uid")
+                let tabbarController = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar") as! TabbarController
+                tabbarController.modalPresentationStyle = .fullScreen
+                self.present(tabbarController, animated: true)
+            }
+        }
         let tabbarController = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar") as! TabbarController
         tabbarController.modalPresentationStyle = .fullScreen
         self.present(tabbarController, animated: true)
--- OnlineCounseling/Controller/RootViewController.swift
+++ OnlineCounseling/Controller/RootViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RootViewController: UIViewController {
 
@@ -15,6 +16,19 @@ class RootViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // ログインしているか認証(認証OK)
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            guard let user = user else {
+                return
+            }
+            print(user.uid, "user.uid")
+            print(user.isAnonymous, "匿名ログインかどうか")
+            print(auth.currentUser?.uid, "現在のユーザーuid")
+        }
     }
 }
 
